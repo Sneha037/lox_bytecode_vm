f6feec2d8559caee116f5901c8ebfc5b3938c19b
Sneha Mukherjee <mukherjeesneha037@gmail.com>
2026-01-16 03:00:02 +0000

    changes made to vm

--- vm.c
+++ vm.c
@@ -6,14 +6,31 @@
 
 VM vm;
 
+static void resetStack()
+{
+    vm.stackTop = vm.stack;
+}
+
 void initVM()
 {
-
+     resetStack();
 }
 
 void freeVM()
 {
+    
+}
 
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop()
+{
+    vm.stackTop--;
+    return *vm.stackTop;
 }
 
 static InterpretResult run()
@@ -24,6 +41,14 @@ static InterpretResult run()
     for(;;)
     {
         #ifdef DEBUG_TRACE_EXECUTION
+           printf("      ");
+           for(Value* slot = vm.stack; slot < vm.stackTop; slot++)
+           {
+              printf("[ ");
+              printValue(*slot);
+              printf(" ]");
+           }
+           printf("\n");
            disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
         #endif
 
@@ -32,11 +57,14 @@ static InterpretResult run()
         {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                printValue(constant);
-                printf("\n");
+                //printValue(constant);
+                //printf("\n");
+                push(constant);
                 break;
             }
             case OP_RETURN:{
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
             }
         }
